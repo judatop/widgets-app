e0f4c46528c30f2486e2a7bf38b3894622a7aec1
judatop <judagarciaclavijo@gmail.com>
2024-02-08 22:38:01 -0500

    [ADD] tutorial route added

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -51,4 +51,10 @@ const appMenuItems = <MenuItem>[
     link: '/ui-controls',
     icon: Icons.car_rental_outlined,
   ),
+  MenuItem(
+    title: 'Introduction to app',
+    subTitle: 'Little tutorial to app',
+    link: '/tutorial',
+    icon: Icons.accessibility_rounded,
+  ),
 ];
